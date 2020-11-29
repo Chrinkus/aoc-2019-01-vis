e2b094e4b17f1fa135cabde78bec2e61214a94b8
chrinkus <seeschickrun@gmail.com>
2020-11-29 12:37:25 -0600

    Added FLTK on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Box.H>
+
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Let's get visualizin'\n";
-
-    for (auto i = 0; i < argc; ++i) {
-        std::cout << argv[i] << '\n';
-    }
+    auto win = new Fl_Double_Window(600, 400, "AOC 2019 Day 1");
+    auto box = new Fl_Box(20, 40, 560, 320, "Hello AOC in FLTK");
+    box->box(FL_UP_BOX);
+    box->labelfont(FL_BOLD | FL_ITALIC);
+    box->labelsize(48);
+    box->labeltype(FL_SHADOW_LABEL);
+    win->end();
+    win->show(argc, argv);
+    return Fl::run();
 }
